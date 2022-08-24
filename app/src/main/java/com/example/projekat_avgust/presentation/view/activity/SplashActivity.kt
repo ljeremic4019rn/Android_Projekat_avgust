1aad6f6d6b5c54630ccf8abbea4e258758a5800a
Luka Jeremic <ljeremic4019rn@raf.rs>
2022-08-24 13:33:16 +0200

    dodat bar i navigation

--- app/src/main/java/com/example/projekat_avgust/presentation/view/activity/SplashActivity.kt
+++ app/src/main/java/com/example/projekat_avgust/presentation/view/activity/SplashActivity.kt
@@ -1,11 +1,13 @@
 package com.example.projekat_avgust.presentation.view.activity
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.projekat_avgust.R
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
