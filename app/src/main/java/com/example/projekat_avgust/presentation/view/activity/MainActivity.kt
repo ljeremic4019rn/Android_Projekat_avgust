6a6f5b8db59a1e16bd06313d0e53b38dd57de802
Luka Jeremic <ljeremic4019rn@raf.rs>
2022-08-25 19:44:40 +0200

    checkpoint

--- app/src/main/java/com/example/projekat_avgust/presentation/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/projekat_avgust/presentation/view/activity/MainActivity.kt
@@ -32,23 +32,16 @@ class MainActivity : AppCompatActivity() {
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
 
-
-
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_users, R.id.nav_create_user), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
-
     }
-
-
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
-
         return true
     }
 
