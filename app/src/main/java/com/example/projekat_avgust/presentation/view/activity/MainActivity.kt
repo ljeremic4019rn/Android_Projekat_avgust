99df1c066ccfbb252bbc2a0e2283d081d1eb1147
Luka Jeremic <ljeremic4019rn@raf.rs>
2022-08-25 19:38:07 +0200

    napravljen logout

--- app/src/main/java/com/example/projekat_avgust/presentation/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/projekat_avgust/presentation/view/activity/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.projekat_avgust.presentation.view.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -30,17 +33,40 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
 
 
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_users, R.id.nav_create_user), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
     }
+
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
+
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.logoutBtn -> {
+            val sharedPreferences = getSharedPreferences(packageName, MODE_PRIVATE)
+
+            sharedPreferences
+                .edit()
+                .putBoolean("rememberMe", false)
+                .apply()
+
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+            finish()
+            true
+        }
+        else -> false
     }
 
     override fun onSupportNavigateUp(): Boolean {
